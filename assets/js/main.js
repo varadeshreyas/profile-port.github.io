049926892d796b0d7136a524a2d80c78ebec9bc3
shreyas0201 <shreyasvarade0201@gmail.com>
2023-04-02 20:42:26 +0530

    Six commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,8 +103,8 @@ tabs.forEach(tab =>{
 
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.service__button'),
-      modalCloses = document.querySelectorAll('.service__modal-close')
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
     
 let modal = function(modalClick){
     modalViews[modalClick].classList.add('active-modal')
